ec88a22781f52d0bde8345c2262e44c0735edfd9
Mark Gerrior <github@gerrior.com>
2020-05-07 09:54:38 -0700

    Tests Pass

--- Quakes/Quakes/Quake.swift
+++ Quakes/Quakes/Quake.swift
@@ -13,6 +13,14 @@ class Quake: NSObject, Decodable {
     
     enum CodingKeys: String, CodingKey {
         case magnitude = "mag"
+        case place
+        case time
+        case latitude
+        case longitude
+
+        case properties
+        case geometry
+        case coordinates
     }
 
     let magnitude: Double
@@ -23,13 +31,19 @@ class Quake: NSObject, Decodable {
 
     required init(from decoder: Decoder) throws {
         // Containers
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let properties = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .properties)
+        let geometry = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .geometry)
+        var coordinates = try geometry.nestedUnkeyedContainer(forKey: .coordinates)
 
         // Set each values here
-        self.magnitude = 0
-        self.place = ""
-        self.time = Date()
-        self.latitude = 0
-        self.longitude = 0
+        self.magnitude = try properties.decode(Double.self, forKey: .magnitude)
+        self.place = try properties.decode(String.self, forKey: .place)
+        self.time = try properties.decode(Date.self, forKey: .time)
+
+        // Longitude is first
+        self.longitude = try coordinates.decode(Double.self)
+        self.latitude = try coordinates.decode(Double.self)
 
         super.init()
     }
