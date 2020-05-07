5ed86e96f9bb556ef8d65a899d549e77b6a8442c
Mark Gerrior <github@gerrior.com>
2020-05-07 15:49:06 -0700

    Custom Annotations

--- Quakes/Quakes/EarthquakesViewController.swift
+++ Quakes/Quakes/EarthquakesViewController.swift
@@ -19,6 +19,10 @@ class EarthquakesViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
+        mapView.delegate = self
+        // MKMarkerAnnotationView like a table view cell
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: "QuakeView")
+
         quakeFetcher.fetchQuakes { (quakes, error) in
             if let error = error {
                 print("Error fetching quakes: \(error)")
@@ -30,7 +34,42 @@ class EarthquakesViewController: UIViewController {
 
             DispatchQueue.main.async {
                 self.mapView.addAnnotations(quakes)
+
+                guard let quake = quakes.first else { return }
+
+                let span = MKCoordinateSpan(latitudeDelta: 2, longitudeDelta: 2)
+                let region = MKCoordinateRegion(center: quake.coordinate, span: span)
+                self.mapView.setRegion(region, animated: true)
+
+                // Future: can call a function to do all this work
             }
         }
 	}
+}
+
+extension EarthquakesViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let quake = annotation as? Quake else {
+            fatalError("Only quakes are supported")
+        }
+
+        guard let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "QuakeView", for: annotation) as? MKMarkerAnnotationView else {
+            fatalError("Missing a registered view")
+        }
+
+        annotationView.glyphImage = UIImage(named: "QuakeIcon")
+        if let mag = quake.magnitude {
+            if mag >= 5.0 {
+                annotationView.markerTintColor = .red
+            } else if mag >= 3.0 && mag < 5.0 {
+                annotationView.markerTintColor = .orange
+            } else {
+                annotationView.markerTintColor = .yellow
+            }
+        } else {
+            annotationView.markerTintColor = .white
+        }
+
+        return annotationView
+    }
 }
